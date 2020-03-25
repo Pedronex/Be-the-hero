24b72d1e39c56b4c42347c539fdb917f4ad5e57c
Pedronex <pedrosoares.nex@gmail.com>
2020-03-25 17:00:08 -0300

    Frontend da aplicação
    Semana OmniStack 11.0
    - Foi adicionado uma validação para o valor em reais na inseção de um novo caso

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './global.css'
+
+import Routes from './routes';
+// JSX = Javascript XML
+
+function App() {
+  // useState() retorna um array [valor, setValor]
+  return (
+    <Routes />
+  );
+}
+
+export default App;
