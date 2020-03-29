7ac93563e8a54749bc55b977750d3410f992f332
Pedronex <pedrosoares.nex@gmail.com>
2020-03-28 21:28:44 -0300

    atualizando api

--- mobile/src/service/api.js
+++ mobile/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.100.7:3333',
+  baseURL: 'https://backend-bethehero-pedronex.herokuapp.com',
 });
 
 export default api;
